1b6c0721d75e10217942a2aba31ef5b3d5850303
Kevin Massongo <kevinmassongo196@gmail.com>
2023-11-16 13:36:35 +0100

    add codes

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputText = document.querySelector('input');
+const sendButton = document.querySelector('button');
+const taskList = document.querySelector('ul');
+
+const checkSpace = (value) =>{
+    return value.trim().length;
+}
+
+
+const createButtonDelete = () => {
+    const newButton = document.createElement('button');
+    const buttonText = document.createElement('img');
+    buttonText.classList.add('img-container')
+    newButton.addEventListener('click',(e)=>{
+        e.preventDefault();
+        e.target.parentElement.remove()
+    })
+    newButton.appendChild(buttonText);
+    return newButton
+}
+
+const createTaskList = (value) =>{
+    const newList = document.createElement('li');
+    const newListText = document.createTextNode(value.trim());
+    const deleteButton = createButtonDelete();
+    newList.appendChild(newListText);
+    newList.appendChild(deleteButton);
+    return newList
+}
+
+const addList = () =>{
+    const inputValue = String(inputText.value);
+    if(checkSpace(inputValue)){
+        const newTaskList = createTaskList(inputValue);
+        taskList.prepend(newTaskList);
+    }
+    inputText.value = ""
+}
+
+sendButton.addEventListener('click',(e)=>{
+    e.preventDefault()
+    addList()
+})
